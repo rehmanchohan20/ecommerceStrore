8a6e73e2e96fe496738c39da80ef89b0e50cadbc
Rehman Chohan <rehmanchohan20@gmail.com>
2025-07-22 18:37:50 +0500

    complete work done from backend with including payment Gateway,

--- src/main/java/com/ecomerce/sell/excepotions/DAOResponse.java
+++ src/main/java/com/ecomerce/sell/excepotions/DAOResponse.java
@@ -14,11 +14,6 @@ public enum DAOResponse {
     ALREADY_EXISTS("VHR_ERROR_08", "Already exists"),
     UNAUTHORIZED("VHR_ERROR_09", "Unauthorized access"),;
 
-  kEYS
-//    #pk_test_51PI4J0RpVeu5Nab9nUra38Gv9qD5N8HU39fO8du0XQlZuIST1X3xH8zinqSebISwxe84QQJUtmpk0uMkaj70LYQd00tBOOcQoN
-//#sk_test_51PI4J0RpVeu5Nab9t29yZO7xzbcZct0RKSQHxPJG4q5DQ5wrGPMjT3T16FzJYQtTtZAm1sNZUxas90cFA3GHmDfW005tKBNiMN
-//#whsec_23a78ccf3d1cf56206bc32100e94d4095cc6038789fda3960fdc4cd9de4805cd
-
     private final String code;
     private final String message;
 
