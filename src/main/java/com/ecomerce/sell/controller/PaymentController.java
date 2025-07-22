2a1c725163b3cedb2f1d0fe576404f225d1c8604
Rehman Chohan <rehmanchohan20@gmail.com>
2025-07-22 18:28:57 +0500

    complete work done from backend with including payment Gateway,

--- src/main/java/com/ecomerce/sell/controller/PaymentController.java
+++ src/main/java/com/ecomerce/sell/controller/PaymentController.java
@@ -68,6 +68,6 @@ public class PaymentController {
 
     @GetMapping("/failure")
     public String failure() {
-        return "failed to make an order please try again later";
+        return "failed to make an order please try again later IN A WHILE";
     }
 }
